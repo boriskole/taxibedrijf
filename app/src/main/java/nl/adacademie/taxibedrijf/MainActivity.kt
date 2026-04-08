36aed7b3123091e78a440c566fa7df0b61c3d148
Boris <boris.kole@outlook.com>
2026-04-08 11:45:38 +0200

    Opdracht #2 gemaakt

--- app/src/main/java/nl/adacademie/taxibedrijf/MainActivity.kt
+++ app/src/main/java/nl/adacademie/taxibedrijf/MainActivity.kt
@@ -1,14 +1,33 @@
 package nl.adacademie.taxibedrijf
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var licensePlateTextField: EditText
+    private lateinit var submitButton: Button
+    private lateinit var resultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        licensePlateTextField = findViewById(R.id.et_kenteken)
+        submitButton = findViewById(R.id.b_search_button)
+        resultTextView = findViewById(R.id.tv_result_text)
+
+        submitButton.setOnClickListener {
+
+            if (licensePlateTextField.text.isNotBlank()) {
+                resultTextView.text = licensePlateTextField.text
+            }
+
+        }
+
     }
+
 }
